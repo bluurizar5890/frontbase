f5a71faec651be52a4b56d065c513ce27f35e3d0
Byron Lopez Urizar <blopez@maycom-ad.maycom.com.gt>
2020-08-28 14:38:30 -0600

    Se actualizo url de api

--- skeleton/src/App/service/conectorApi.js
+++ skeleton/src/App/service/conectorApi.js
@@ -1,8 +1,8 @@
-const BASE_URL = "http://104.155.131.229/api";
-//const BASE_URL = "http://127.0.0.1:3500/api";
+// const BASE_URL = "http://104.155.131.229/api";
+const BASE_URL = "http://127.0.0.1:3000/api";
 const callApi = async (endpoint, options = {}) => {
     options.headers = {
-        'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzdWFyaW9JZCI6MX0sImlhdCI6MTU5NDUwOTAzOH0.0KivofSLCHDtqd_6-GauPlIhailkv1OcgleO3XkrooA`,
+        'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzdWFyaW9JZCI6MX0sImlhdCI6MTU5ODY0NTkwOH0.D3CbE9CdG_kjliv8F3jjSf4dnNy0BvpqPntYhbfHVXI`,
         'Content-Type': 'application/json',
         Accept: 'application/json',
     };
